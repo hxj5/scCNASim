c82b87990b9cd581a82b5e4a0f035ba9059b73c4
Rongting Huang <rthuang@foxmail.com>
2024-10-07 23:22:28 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ here = path.abspath(path.dirname(__file__))
 exec(open("./sccnvsim/app.py").read())
 
 # Get the long description from the relevant file
-with open(path.join(here, "README.md"), encoding='utf-8') as f:
+with open(path.join(here, "README.rst"), encoding='utf-8') as f:
     long_description = f.read()
 
 # set "numpy<2"?
